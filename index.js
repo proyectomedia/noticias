1d3c9f96ce34cbe88e91c054aacb0ddfd34cbbd8
Jose Toro <toronacii@gmail.com>
2016-10-24 08:26:17 -0400

    Anadir solo noticias nuevas

--- index.js
+++ index.js
@@ -10,11 +10,33 @@ var newsRef = firebaseApp.database().ref('news');
 schedule.scheduleJob('*/10 * * * * *', () => {
 
     scrap()
-        .then(news => news.forEach(_new => newsRef.child(_new._id).set(_new)))
+        .then(news => news.forEach(_new => {
+
+            newsRef.orderByKey().equalTo(_new._id).once('value', snap => {
+
+                if (!snap.val()) {
+                    newsRef.child(_new._id).set(_new);
+                }
+
+            })
+
+        }))
         .catch(err => {
 
             console.error(err);
 
         });
 
+});
+
+newsRef.on('child_added', snap => {
+    console.log("Added " + snap.key);
+});
+
+newsRef.on('child_changed', snap => {
+    console.log("Changed " + snap.key);
+});
+
+newsRef.on('child_removed', snap => {
+    console.log("Removed " + snap.key);
 });
