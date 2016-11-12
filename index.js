c56a9b835274e3c3b3a2c4dad688eda089ff9f73
Kellerman Rivero <krsloco@gmail.com>
2016-11-12 11:18:48 -0400

    Fixed index.js

--- index.js
+++ index.js
@@ -8,61 +8,61 @@ var fbApi = require('./lib/apiFace');
 var f=require('./facebook/fbpages');
 var scraperfacebook=require('./facebook/index');
 
-// //facebook extaer noticias-> funcionan ok
-//  schedule.scheduleJob('0 */5 * * * *',()=>{
-//    console.log("");
-//    console.log("Entro a 5minutos");
-//    scraperfacebook.sacarpostyguardar(f.paginas5minutos)
-//    .catch(function (err) {
-//      console.log("Error:"+ err)
-//     })
-//  });
+//facebook extaer noticias-> funcionan ok
+ schedule.scheduleJob('0 */5 * * * *',()=>{
+   console.log("");
+   console.log("Entro a 5minutos");
+   scraperfacebook.sacarpostyguardar(f.paginas5minutos)
+   .catch(function (err) {
+     console.log("Error:"+ err)
+    })
+ });
 
-//  schedule.scheduleJob('0 */15 * * * *',()=>{
-//    console.log("");
-//    console.log("Entro a 15minutos");
-//    scraperfacebook.sacarpostyguardar(f.paginas15minutos)
-//    .catch(function (err) {
-//      console.log("Error:"+ err)
-//     })
-//  });
+ schedule.scheduleJob('0 */15 * * * *',()=>{
+   console.log("");
+   console.log("Entro a 15minutos");
+   scraperfacebook.sacarpostyguardar(f.paginas15minutos)
+   .catch(function (err) {
+     console.log("Error:"+ err)
+    })
+ });
 
-//  schedule.scheduleJob('0 0 */1 * * *',()=>{
-//    console.log("");
-//    console.log("Entro a 60minutos");
-//    scraperfacebook.sacarpostyguardar(f.paginas60minutos)
-//    .catch(function (err) {
-//      console.log("Error:"+ err)
-//     })
-//  });
-
-
-// //Scraper web
-//  var pages = config.pages.filter(page => page.active && page.schedule);
-//  pages.forEach(page => {
-//      page.schedule.forEach(pageSchedule => schedule.scheduleJob(pageSchedule, () => scrapping.fetchAndSave(page.url)))
-//  });
+ schedule.scheduleJob('0 0 */1 * * *',()=>{
+   console.log("");
+   console.log("Entro a 60minutos");
+   scraperfacebook.sacarpostyguardar(f.paginas60minutos)
+   .catch(function (err) {
+     console.log("Error:"+ err)
+    })
+ });
 
 
+//Scraper web
+ var pages = config.pages.filter(page => page.active && page.schedule);
+ pages.forEach(page => {
+     page.schedule.forEach(pageSchedule => schedule.scheduleJob(pageSchedule, () => scrapping.fetchAndSave(page.url)))
+ });
 
 
-// //Publicar noticias
-//  f.page.forEach(page => {
-//   schedule.scheduleJob(page.schedule, () => {
-//       scrapping
-//               .getRecentNews(1, page.tipo)
-//               .then(news => {
-//                   news.forEach(newsItem => {
-//                        fbApi.postNews(page.id, newsItem, (n) => scrapping.markAsPublished(n, page.tipo))
-//                    })
-//               });
-//   });
-// })
+
+
+//Publicar noticias
+ f.page.forEach(page => {
+  schedule.scheduleJob(page.schedule, () => {
+      scrapping
+              .getRecentNews(1, page.tipo)
+              .then(news => {
+                  news.forEach(newsItem => {
+                       fbApi.postNews(page.id, newsItem, (n) => scrapping.markAsPublished(n, page.tipo))
+                   })
+              });
+  });
+})
 
 //Pruebas aca abajo
 
 
-scrapping.fetchAndSave("http://diariooficial.elperuano.pe/Normas");
+//scrapping.fetchAndSave("http://diariooficial.elperuano.pe/Normas");
          //.then(news => news.forEach(n => scrapping.markAsPublished(n, 'politica')));
 
 
